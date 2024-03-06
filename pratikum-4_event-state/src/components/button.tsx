33adca07fcc66c0c1eee4a25bdecb9bf162afd63
novalzahwanfhalma <novalzf29@gmail.com>
2024-03-06 10:02:28 +0700

    pratikum-3_langkah-2

--- pratikum-4_event-state/src/components/button.tsx
+++ pratikum-4_event-state/src/components/button.tsx
@@ -10,7 +10,8 @@ export function Tombol_2({isiPesan, namaTombol}) {
 export function Tombol_3({isiPesan, namaTombol}) {
     return (
       <button className="bg-green-500 hover:bg-green-700 text-white p-2 rounded m-2"
-      onClick={() => {
+      onClick={(e) => {
+        e.stopPropagation();
         alert(isiPesan)
     }
     }>
