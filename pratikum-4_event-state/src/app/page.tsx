f2b86c62e51eee26089916a1d3ef0935a714bc6d
novalzahwanfhalma <novalzf29@gmail.com>
2024-03-06 09:50:55 +0700

    pratikum-2

--- pratikum-4_event-state/src/app/page.tsx
+++ pratikum-4_event-state/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Tombol_l from "@/components/button";
+import Tombol_l, { Tombol_2 } from "@/components/button";
 
 export default function Home() {
 return (
@@ -7,6 +7,8 @@ return (
   <div className="container mx-auto">
     <h2>Kuis Kota</h2>
     <Tombol_l />
+    <hr></hr>
+    <Tombol_2 isiPesan="Ini Pesanku" namaTombol="Pesan" />
   </div>
   </>
 );
--- pratikum-4_event-state/src/components/button.tsx
+++ pratikum-4_event-state/src/components/button.tsx
@@ -1,3 +1,12 @@
+export function Tombol_2({isiPesan, namaTombol}) {
+    return (
+      <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+      onClick={() => alert(isiPesan)}>
+        {namaTombol}
+      </button>
+    );
+}
+
 export default function Tombol_l() {
     function HandleClick() {
         alert("Tombol telah di tekan!");
