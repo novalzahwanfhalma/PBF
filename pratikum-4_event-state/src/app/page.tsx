dcd759f8b161e964f777dea5dd28aad92ad81007
novalzahwanfhalma <novalzf29@gmail.com>
2024-03-06 08:41:04 +0700

    pratikum-1_langkah-1

---
+++ pratikum-4_event-state/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Tombol_l from "@/components/button";
+
+export default function Home() {
+return (
+  <>
+  <div className="container mx-auto">
+    <h2>Kuis Kota</h2>
+    <Tombol_l />
+  </div>
+  </>
+);
+}
---
+++ pratikum-4_event-state/src/components/button.tsx
@@ -0,0 +1,7 @@
+export default function Tombol_l() {
+    return (
+    <button className= "bg-blue-500 hover: bg-blue-700 text-white p-2 rounded">
+    Ini tombol
+    </button>
+    );
+}
