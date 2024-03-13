2c37dbb36cd97ac04973a9e96d3a6c8eb6fd519c
novalzahwanfhalma <novalzf29@gmail.com>
2024-03-13 10:05:56 +0700

    pratikum-1

---
+++ pratikum-5_route/routing-demo/README.md
@@ -0,0 +1,10 @@
+Pratikum 1 : Membuat routing sederhana
+
+Home
+![Soal-1](images/home.png)
+
+About
+![Soal-2](images/about.png)
+
+profile
+![Soal-3](images/profile.png)
---
+++ pratikum-5_route/routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+    return ( 
+    <div>
+        <h1>Biodata saya:</h1> 
+            <ul>
+                <li><strong>Nama :</strong>NOVAL ZAHWAN FHALMA</li>
+                <li><strong>Nim :</strong>2341728029</li>
+                <li><strong>Kelas :</strong>TI 3 C</li>
+                <li><strong>Jurusan :</strong>TEKNOLOGI INFORMASI</li>
+                <li><strong>Umur :</strong>19</li>
+            </ul> 
+    </div>
+    );
+}
